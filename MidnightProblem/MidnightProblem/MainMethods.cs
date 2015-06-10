49fc0d102544ac320c24fa2cf4c4d9d9804f6754
Qerts <michal.balicky@gmail.com>
2015-06-10 14:02:41 +0200

    Partial initial code

---
+++ MidnightProblem/MidnightProblem/MainMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidnightProblem
+{
+    public static class MainMethods
+    {
+        public static void PrintText(string text)
+        {
+        
+        }
+
+    }
+}
---
+++ MidnightProblem/MidnightProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidnightProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
